787029b4a3d465f687444a6c7a2fe9792fa10a38
john <jonhmiguel138@gmail.com>
2026-07-18 23:46:57 -0300

    sandbox: js fundamentals practice (loops, conditionals, nullish coalescing)

---
+++ sandbox/javascript/js-fundamentals.js
@@ -0,0 +1,7 @@
+const dinosaurs = [
+  { name: "T-Rex", diet: "Carnivore", size: 12 },
+  { name: "Brachiosaurus", diet: "Herbivore", size: 26 },
+  { name: "Velociraptor", diet: "Carnivore", size: 1.8 },
+  { name: "Triceratops", diet: "Herbivore", size: 9 },
+  { name: "Spinosaurus", diet: null, size: 15 },
+];
